e82d1c6ffaa1b16f0e62b131ec3411708f8b3d7e
franky <frankyzf94@gmail.com>
2017-04-08 23:29:37 +0800

    add kappa

--- usefulness_annotation/data/src/kappa.py
+++ usefulness_annotation/data/src/kappa.py
@@ -1,6 +1,7 @@
 __author__ = 'franky'
 
 from sklearn.metrics import cohen_kappa_score
+from sklearn.metrics import confusion_matrix
 from collections import defaultdict
 import numpy as np
 
@@ -25,6 +26,72 @@ tasks = []
 for i in range(1, 3):
     if i != 17:
         tasks.append(str(i))
+
+
+def weighted_cohen_kappa_score(y1, y2, labels=None, weights=None):
+    """Cohen's kappa: a statistic that measures inter-annotator agreement.
+    This function computes Cohen's kappa [1]_, a score that expresses the level
+    of agreement between two annotators on a classification problem. It is
+    defined as
+    .. math::
+        \kappa = (p_o - p_e) / (1 - p_e)
+    where :math:`p_o` is the empirical probability of agreement on the label
+    assigned to any sample (the observed agreement ratio), and :math:`p_e` is
+    the expected agreement when both annotators assign labels randomly.
+    :math:`p_e` is estimated using a per-annotator empirical prior over the
+    class labels [2]_.
+    Read more in the :ref:`User Guide <cohen_kappa>`.
+    Parameters
+    ----------
+    y1 : array, shape = [n_samples]
+        Labels assigned by the first annotator.
+    y2 : array, shape = [n_samples]
+        Labels assigned by the second annotator. The kappa statistic is
+        symmetric, so swapping ``y1`` and ``y2`` doesn't change the value.
+    labels : array, shape = [n_classes], optional
+        List of labels to index the matrix. This may be used to select a
+        subset of labels. If None, all labels that appear at least once in
+        ``y1`` or ``y2`` are used.
+    weights : str, optional
+        List of weighting type to calculate the score. None means no weighted;
+        "linear" means linear weighted; "quadratic" means quadratic weighted.
+    Returns
+    -------
+    kappa : float
+        The kappa statistic, which is a number between -1 and 1. The maximum
+        value means complete agreement; zero or lower means chance agreement.
+    References
+    ----------
+    .. [1] J. Cohen (1960). "A coefficient of agreement for nominal scales".
+           Educational and Psychological Measurement 20(1):37-46.
+           doi:10.1177/001316446002000104.
+    .. [2] `R. Artstein and M. Poesio (2008). "Inter-coder agreement for
+           computational linguistics". Computational Linguistics 34(4):555-596.
+           <http://www.mitpressjournals.org/doi/abs/10.1162/coli.07-034-R2#.V0J1MJMrIWo>`_
+    .. [3] `Wikipedia entry for the Cohen's kappa.
+            <https://en.wikipedia.org/wiki/Cohen%27s_kappa>`_
+    """
+    confusion = confusion_matrix(y1, y2, labels=labels)
+    n_classes = confusion.shape[0]
+    sum0 = np.sum(confusion, axis=0)
+    sum1 = np.sum(confusion, axis=1)
+    expected = np.outer(sum0, sum1) / np.sum(sum0)
+
+    if weights is None:
+        w_mat = np.ones([n_classes, n_classes], dtype=np.float)
+        w_mat.flat[:: n_classes + 1] = 0
+    elif weights == "linear" or weights == "quadratic":
+        w_mat = np.zeros([n_classes, n_classes], dtype=np.float)
+        w_mat += np.arange(n_classes)
+        if weights == "linear":
+            w_mat = np.abs(w_mat - w_mat.T)
+        else:
+            w_mat = (w_mat - w_mat.T) ** 2
+    else:
+        raise ValueError("Unknown kappa weighting type.")
+
+    k = np.sum(w_mat * confusion) / np.sum(w_mat * expected)
+    return 1 - k
 
 
 def fleiss_kappa(table):
@@ -94,6 +161,7 @@ def usefulness_kappa():
                 if len(assessors_annotations[assessor_i]) == len(assessors_annotations[assessor_j]):
             #items.append(cohen_kappa_score(annotation_i, annotation_j, ['1','2','3','4']))
             #items.append(cohen_kappa_score(annotation_i, annotation_j, [0,1]))
+                    #print 'task:'+task, '\tusefulness cohen:', assessor_i, assessor_j, weighted_cohen_kappa_score(assessors_annotations[assessor_i], assessors_annotations[assessor_j], ['1','2','3','4'], weights="quadratic")
                     print 'task:'+task, '\tusefulness cohen:', assessor_i, assessor_j, cohen_kappa_score(assessors_annotations[assessor_i], assessors_annotations[assessor_j], ['1','2','3','4'])
                     #print 'usefulness cohen', annotations.keys()[i], annotations.keys()[j], cohen_kappa_score(annotations[annotations.keys()[i]], annotations[annotations.keys()[j]], [0, 1])
             #print np.mean(items)
@@ -136,11 +204,11 @@ def satisfaction_kappa():
     fin = open("../anno_tasksatisfaction.csv").readlines()[1:]
     for line in fin:
         id,assessor_id,session_id,score = line.strip().split(',')
-        satisfactions[session_id][assessor_id] = score
-        '''if int(score) > 4:
+        #satisfactions[session_id][assessor_id] = score
+        if int(score) > 4:
             satisfactions[session_id][assessor_id] = 1
         else:
-            satisfactions[session_id][assessor_id] = 0'''
+            satisfactions[session_id][assessor_id] = 0
 
     for task in tasks:
         assessors_annotations = defaultdict(lambda: [])
@@ -155,8 +223,9 @@ def satisfaction_kappa():
                 if len(assessors_annotations[assessor_i]) == len(assessors_annotations[assessor_j]):
         #items.append(cohen_kappa_score(annotation_i, annotation_j, ['1','2','3','4','5']))
         #items.append(cohen_kappa_score(annotation_i, annotation_j, [0,1]))
-                    print 'task:'+task, '\tsatisfaction cohen', assessor_i, assessor_j, cohen_kappa_score(assessors_annotations[assessor_i], assessors_annotations[assessor_j], ['1','2','3','4','5'])
-                #print 'satisfaction', annotations.keys()[i], annotations.keys()[j], cohen_kappa_score(annotations[annotations.keys()[i]], annotations[annotations.keys()[j]], [0, 1])
+                    #print 'task:'+task, '\tsatisfaction cohen', assessor_i, assessor_j, weighted_cohen_kappa_score(assessors_annotations[assessor_i], assessors_annotations[assessor_j], ['1','2','3','4','5'], "quadratic")
+                    #print 'task:'+task, '\tsatisfaction cohen', assessor_i, assessor_j, cohen_kappa_score(assessors_annotations[assessor_i], assessors_annotations[assessor_j], ['1','2','3','4','5'])
+                    print 'task:'+task, '\tsatisfaction cohen', assessor_i, assessor_j, cohen_kappa_score(assessors_annotations[assessor_i], assessors_annotations[assessor_j], [0, 1])
         #print np.mean(items)
 
 
@@ -166,6 +235,10 @@ def satisfactioin_fleiss_kappa():
     assessors = set()
     for line in fin:
         id,assessor_id,session_id,score = line.strip().split(',')
+        if int(score) <= 4:
+            score = '0'
+        if score == '5':
+            score = '1'
         satisfactions[session_id][assessor_id] = int(score)
         assessors.add(assessor_id)
     assessors_list = []
@@ -174,7 +247,7 @@ def satisfactioin_fleiss_kappa():
     annotations_table = []
 
     for task in tasks:
-        annotations_array = np.zeros((43, 5))
+        annotations_array = np.zeros((43, 2))
         i = 0
         for session_id in task_to_sessions_dict[task]:
             for assessor in satisfactions[session_id].keys():
