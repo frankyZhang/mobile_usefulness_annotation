20302e05cb0815421791ac3a1cfa9a5188c0b699
franky <frankyzf94@gmail.com>
2017-04-08 11:52:24 +0800

    2 tasks

--- usefulness_annotation/data/src/kappa.py
+++ usefulness_annotation/data/src/kappa.py
@@ -5,6 +5,28 @@ from collections import defaultdict
 import numpy as np
 
 
+assessor_groups = [
+    ['2016211001', '2013011392', '2013012195'],
+    ['2014011426', '2014011325', '2016011258']
+]
+
+
+def task_to_sessions():
+    task_to_sessions_dict = defaultdict(lambda: set())
+    fin = open("../../temp/annotation_sequence.tsv").readlines()[1:]
+    for line in fin:
+        id,session_id,user_id,setting_id,task_id,source_id,annotation_index,result_id,timestamp,is_clicked,click_dwell_time,clicked_url,weighted_exposed_time = line.strip().split('\t')
+        task_to_sessions_dict[task_id].add(session_id)
+    return task_to_sessions_dict
+
+
+task_to_sessions_dict = task_to_sessions()
+tasks = []
+for i in range(1, 3):
+    if i != 17:
+        tasks.append(str(i))
+
+
 def fleiss_kappa(table):
     '''Fleiss' kappa multi-rater agreement measure
 
@@ -50,35 +72,38 @@ def fleiss_kappa(table):
 def usefulness_kappa():
     items = []
     usefulnesses = defaultdict(lambda: defaultdict(lambda: {}))
-    fout = open("../anno_usefulness.csv").readlines()[1:]
-    for line in fout:
+    fin = open("../anno_usefulness.csv").readlines()[1:]
+    for line in fin:
         id,assessor_id,session_id,index,score = line.strip().split(',')
         usefulnesses[session_id][index][assessor_id] = score
         '''if int(score) > 2:
             usefulnesses[session_id][index][assessor_id] = 1
         else:
             usefulnesses[session_id][index][assessor_id] = 0'''
-    annotations = defaultdict(lambda: [])
-    for session_id in usefulnesses.keys():
-        for index in usefulnesses[session_id].keys():
-            for assessor_id in usefulnesses[session_id][index].keys():
-                annotations[assessor_id].append(usefulnesses[session_id][index][assessor_id])
+    for task in tasks:
+        assessors_annotations = defaultdict(lambda: [])
+        for session_id in task_to_sessions_dict[task]:
+            for index in usefulnesses[session_id].keys():
+                for assessor_id in usefulnesses[session_id][index].keys():
+                    assessors_annotations[assessor_id].append(usefulnesses[session_id][index][assessor_id])
 
-    for i in range(len(annotations.keys())):
-        for j in range(i+1, len(annotations.keys())):
-
-    #items.append(cohen_kappa_score(annotation_i, annotation_j, ['1','2','3','4']))
-    #items.append(cohen_kappa_score(annotation_i, annotation_j, [0,1]))
-            print 'usefulness cohen', annotations.keys()[i], annotations.keys()[j], cohen_kappa_score(annotations[annotations.keys()[i]], annotations[annotations.keys()[j]], ['1','2','3','4'])
-            #print 'usefulness cohen', annotations.keys()[i], annotations.keys()[j], cohen_kappa_score(annotations[annotations.keys()[i]], annotations[annotations.keys()[j]], [0, 1])
-    #print np.mean(items)
+        for i in range(len(assessors_annotations.keys())):
+            for j in range(i+1, len(assessors_annotations.keys())):
+                assessor_i = assessors_annotations.keys()[i]
+                assessor_j = assessors_annotations.keys()[j]
+                if len(assessors_annotations[assessor_i]) == len(assessors_annotations[assessor_j]):
+            #items.append(cohen_kappa_score(annotation_i, annotation_j, ['1','2','3','4']))
+            #items.append(cohen_kappa_score(annotation_i, annotation_j, [0,1]))
+                    print 'task:'+task, '\tusefulness cohen:', assessor_i, assessor_j, cohen_kappa_score(assessors_annotations[assessor_i], assessors_annotations[assessor_j], ['1','2','3','4'])
+                    #print 'usefulness cohen', annotations.keys()[i], annotations.keys()[j], cohen_kappa_score(annotations[annotations.keys()[i]], annotations[annotations.keys()[j]], [0, 1])
+            #print np.mean(items)
 
 
 def usefulness_fleiss_kappa():
     usefulnesses = defaultdict(lambda: defaultdict(lambda: {}))
-    fout = open("../anno_usefulness.csv").readlines()[1:]
+    fin = open("../anno_usefulness.csv").readlines()[1:]
     assessors = set()
-    for line in fout:
+    for line in fin:
         id,assessor_id,session_id,index,score = line.strip().split(',')
         usefulnesses[session_id][index][assessor_id] = int(score)
         assessors.add(assessor_id)
@@ -87,54 +112,59 @@ def usefulness_fleiss_kappa():
         assessors_list.append(assessor)
     annotations_table = []
 
-    sub_num = 0
-    for session_id in usefulnesses.keys():
-        for index in usefulnesses[session_id].keys():
-            sub_num += 1
+    for task in tasks:
+        sub_num = 0
+        for session_id in task_to_sessions_dict[task]:
+            for index in usefulnesses[session_id].keys():
+                sub_num += 1
 
-    annotations_array = np.zeros((sub_num, 4))
-    i = 0
-    for session_id in usefulnesses.keys():
-        for index in usefulnesses[session_id].keys():
-            for assessor in assessors_list:
-                annotations_array[i][usefulnesses[session_id][index][assessor]-1] += 1
-            i += 1
+        annotations_array = np.zeros((sub_num, 4))
+        i = 0
+        for session_id in task_to_sessions_dict[task]:
+            for index in usefulnesses[session_id].keys():
+                for assessor in usefulnesses[session_id][index].keys():
+                    annotations_array[i][usefulnesses[session_id][index][assessor]-1] += 1
+                i += 1
 
-    #print np.matrix(annotations_table)
-    print 'usefulness fleiss', fleiss_kappa(annotations_array)
+        #print np.matrix(annotations_table)
+        print 'task:'+task, '\tusefulness fleiss:', fleiss_kappa(annotations_array)
 
 
 def satisfaction_kappa():
     items = []
     satisfactions = defaultdict(lambda: {})
-    fout = open("../anno_tasksatisfaction.csv").readlines()[1:]
-    for line in fout:
+    fin = open("../anno_tasksatisfaction.csv").readlines()[1:]
+    for line in fin:
         id,assessor_id,session_id,score = line.strip().split(',')
         satisfactions[session_id][assessor_id] = score
         '''if int(score) > 4:
             satisfactions[session_id][assessor_id] = 1
         else:
             satisfactions[session_id][assessor_id] = 0'''
-    annotations = defaultdict(lambda: [])
-    for session_id in satisfactions.keys():
-        for assessor_id in satisfactions[session_id].keys():
-            annotations[assessor_id].append(satisfactions[session_id][assessor_id])
 
-    for i in range(len(annotations.keys())):
-        for j in range(i+1, len(annotations.keys())):
+    for task in tasks:
+        assessors_annotations = defaultdict(lambda: [])
+        for session_id in task_to_sessions_dict[task]:
+            for assessor_id in satisfactions[session_id].keys():
+                assessors_annotations[assessor_id].append(satisfactions[session_id][assessor_id])
 
-    #items.append(cohen_kappa_score(annotation_i, annotation_j, ['1','2','3','4','5']))
-    #items.append(cohen_kappa_score(annotation_i, annotation_j, [0,1]))
-            print 'satisfaction cohen', annotations.keys()[i], annotations.keys()[j], cohen_kappa_score(annotations[annotations.keys()[i]], annotations[annotations.keys()[j]], ['1','2','3','4','5'])
-            #print 'satisfaction', annotations.keys()[i], annotations.keys()[j], cohen_kappa_score(annotations[annotations.keys()[i]], annotations[annotations.keys()[j]], [0, 1])
-    #print np.mean(items)
+        for i in range(len(assessors_annotations.keys())):
+            for j in range(i+1, len(assessors_annotations.keys())):
+                assessor_i = assessors_annotations.keys()[i]
+                assessor_j = assessors_annotations.keys()[j]
+                if len(assessors_annotations[assessor_i]) == len(assessors_annotations[assessor_j]):
+        #items.append(cohen_kappa_score(annotation_i, annotation_j, ['1','2','3','4','5']))
+        #items.append(cohen_kappa_score(annotation_i, annotation_j, [0,1]))
+                    print 'task:'+task, '\tsatisfaction cohen', assessor_i, assessor_j, cohen_kappa_score(assessors_annotations[assessor_i], assessors_annotations[assessor_j], ['1','2','3','4','5'])
+                #print 'satisfaction', annotations.keys()[i], annotations.keys()[j], cohen_kappa_score(annotations[annotations.keys()[i]], annotations[annotations.keys()[j]], [0, 1])
+        #print np.mean(items)
 
 
 def satisfactioin_fleiss_kappa():
     satisfactions = defaultdict(lambda: {})
-    fout = open("../anno_tasksatisfaction.csv").readlines()[1:]
+    fin = open("../anno_tasksatisfaction.csv").readlines()[1:]
     assessors = set()
-    for line in fout:
+    for line in fin:
         id,assessor_id,session_id,score = line.strip().split(',')
         satisfactions[session_id][assessor_id] = int(score)
         assessors.add(assessor_id)
@@ -143,15 +173,16 @@ def satisfactioin_fleiss_kappa():
         assessors_list.append(assessor)
     annotations_table = []
 
-    annotations_array = np.zeros((43, 5))
-    i = 0
-    for session_id in satisfactions.keys():
-        for assessor in assessors_list:
-            annotations_array[i][satisfactions[session_id][assessor]-1] += 1
-        i += 1
+    for task in tasks:
+        annotations_array = np.zeros((43, 5))
+        i = 0
+        for session_id in task_to_sessions_dict[task]:
+            for assessor in satisfactions[session_id].keys():
+                annotations_array[i][satisfactions[session_id][assessor]-1] += 1
+            i += 1
 
-    #print np.matrix(annotations_table)
-    print 'satisfaction fleiss', fleiss_kappa(annotations_array)
+        #print np.matrix(annotations_table)
+        print 'task:'+task, '\tsatisfaction fleiss', fleiss_kappa(annotations_array)
 
 
 if __name__ == "__main__":
